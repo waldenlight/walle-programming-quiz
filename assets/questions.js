a8a47ac80eca05f5cdb471c6ae2ceff3af75a2cb
waldenlight <76961678+waldenlight@users.noreply.github.com>
2022-12-31 12:56:28 -0700

    Add timer functionality

--- assets/questions.js
+++ assets/questions.js
@@ -5,64 +5,97 @@ var answerTwo = document.querySelector("#answer-two");
 var answerThree = document.querySelector("#answer-three");
 var answerFour = document.querySelector("#answer-four");
 var result = document.querySelector("#result")
+var timer = document.querySelector("#timer")
 
-var currentQuestion = 1;
 
-// Define Correct/Incorrect Functionality
+function quiz() {
+    // Timer
+    var timeLeft = 75;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timer.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            timer.textContent = '';
+            clearInterval(timeInterval);
+            // End quiz
+            endQuiz();
+        }
+    }, 1000);
 
-var correct = function () {
-    result.textContent = "Correct";
-    currentQuestion += 1;
-    quiz();
-};
-
-var incorrect = function () {
-    result.textContent = "Incorrect";
-    currentQuestion += 1;
-    // Decrease time
-
-    quiz();
-};
-
-// Define Questions
-var questionOne = function () {
-    // Set content
-    question.textContent = "Which of the following is NOT a Javascript datatype?";
-    answerOne.textContent = "conditional";
-    answerTwo.textContent = "int";
-    answerThree.textContent = "string";
-    answerFour.textContent = "boolean";
-    // Assign correct/incorrect answers
-    answerOne.setAttribute("class", "correct");
-    // Run correct/incorrect if clicked
-    answerOne.addEventListener("click", correct);
-    answerTwo.addEventListener("click", incorrect);
-    answerThree.addEventListener("click", incorrect);
-    answerFour.addEventListener("click", incorrect);
-}
-
-var questionTwo = function () {
-    // Set content
-    question.textContent = "Arrays in Javascript can be used to store...";
-    answerOne.textContent = "integers";
-    answerTwo.textContent = "arrays";
-    answerThree.textContent = "strings";
-    answerFour.textContent = "All the above";
-    // Assign correct/incorrect answers
-    answerFour.setAttribute("class", "correct");
-    // Run correct/incorrect if clicked
-    answerOne.addEventListener("click", incorrect);
-    answerTwo.addEventListener("click", incorrect);
-    answerThree.addEventListener("click", incorrect);
-    answerFour.addEventListener("click", correct);
-}
-
-var quiz = function () {
-    if (currentQuestion === 1) {
-        questionOne();
-    } else if (currentQuestion === 2) {
-        questionTwo();
+    // Correct functionality
+    var correct = function () {
+        result.textContent = "Correct";
+        currentQuestion += 1;
+        setInterval(function () {
+            result.textContent = "";
+            clearInterval();
+            questionOn();
+        }, 2000);
     };
+
+    // Incorrect functionality
+    var incorrect = function () {
+        result.textContent = "Incorrect";
+        // Decrease time
+        timeLeft -= 15;
+        currentQuestion += 1;
+        setInterval(function () {
+            result.textContent = "";
+            clearInterval();
+            questionOn();
+        }, 2000);
+    };
+
+    var currentQuestion = 1;
+
+    // Define Questions
+    var questionOne = function () {
+        // Set content
+        question.textContent = "Which of the following is NOT a Javascript datatype?";
+        answerOne.textContent = "conditional";
+        answerTwo.textContent = "int";
+        answerThree.textContent = "string";
+        answerFour.textContent = "boolean";
+        // Assign correct answer(s)
+        answerOne.setAttribute("class", "correct");
+        // Run correct/incorrect if clicked
+        answerOne.addEventListener("click", correct);
+        answerTwo.addEventListener("click", incorrect);
+        answerThree.addEventListener("click", incorrect);
+        answerFour.addEventListener("click", incorrect);
+    }
+
+    var questionTwo = function () {
+        // Set content
+        question.textContent = "Arrays in Javascript can be used to store...";
+        answerOne.textContent = "integers";
+        answerTwo.textContent = "arrays";
+        answerThree.textContent = "strings";
+        answerFour.textContent = "All the above";
+        // Assign correct answer(s)
+        answerFour.setAttribute("class", "correct");
+        // Run correct/incorrect if clicked
+        answerOne.addEventListener("click", incorrect);
+        answerTwo.addEventListener("click", incorrect);
+        answerThree.addEventListener("click", incorrect);
+        answerFour.addEventListener("click", correct);
+    }
+
+    // Determines question user is on
+    var questionOn = function () {
+        if (currentQuestion === 1) {
+            questionOne();
+        } else if (currentQuestion === 2) {
+            questionTwo();
+        };
+    };
+
+    questionOn();
+}
+
+var endQuiz = function () {
+    // end functionality
 };
 
 quiz();
